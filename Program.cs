87046d246dbf2bd4285ff0a4efcfe7bd656b2162
Alex35435 <ryabtsev-alex@mail.ru>
2020-06-18 05:44:50 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace Почтовый_клиент
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MailAddress from = new MailAddress("address@mail.com","NAME");
+            MailAddress to = new MailAddress("address@yandex.ru");
+
+            MailMessage objectMail = new MailMessage(from,to);
+            objectMail.Subject = "Текст";
+            objectMail.Body = "<h2>Письмо-текст работы smp-клиента</h2>";
+            objectMail.IsBodyHtml = true;
+
+            SmtpClient smpt = new SmtpClient("smpt.gmail.com", 587);
+
+            smpt.Credentials = new NetworkCredential("address@gmail.com","mypassword");
+            smpt.EnableSsl = true;
+            smpt.Send(objectMail);
+            Console.Read();
+        }
+    }
+}
